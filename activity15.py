db668b2f82374d1e383b13cf3bd6548814b547bf
Bruhbro22 <donmadhu411@gmail.com>
2026-06-08 18:04:09 -0500

    Add files via upload

---
+++ activity15.py
@@ -0,0 +1,6 @@
+num=int(input("What is your number?: "))
+print("I want to see if that number is even or odd")
+if num%2==0:
+    print("Your number is even")
+else:
+    print("Your number is odd")
